d52a44a57e4779284980f7bc111dc76550f14ab8
HiDiHo <96914811+HiDiHo01@users.noreply.github.com>
2024-10-31 21:09:49 +0100

    Update __init__.py

--- python_frank_energie/__init__.py
+++ python_frank_energie/__init__.py
@@ -1,10 +1,12 @@
 """Frank Energie API library."""
 # python_frank_energie/__init__.py
+from .frank_energie import FrankEnergie
 from .models import Price, PriceData
 from .authentication import Authentication
 from .exceptions import AuthException, ConnectionException
 
 __all__ = [
+    "FrankEnergie",
     "Authentication",
     "AuthException",
     "ConnectionException",
