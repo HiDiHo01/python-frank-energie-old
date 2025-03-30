de108c53eb7ea2ec3fbfbe38ea076e0947da54ee
Roy <96914811+HiDiHo01@users.noreply.github.com>
2025-03-30 23:51:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-frank-energie",  # Package name (must be unique on PyPI)
-    version="2025.3.30",
+    version="2025.3.31",
     description="Asynchronous Python package for Frank Energie. Retrieve energy prices for Frank energie.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
